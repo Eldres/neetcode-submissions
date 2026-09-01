6496aba95e2d7e6f5347b3baef3fa64256b65d7b
Josh Nagel <nageljp27@gmail.com>
2026-09-01 12:45:11 -0600

    Add: number-of-students-unable-to-eat-lunch - submission-1

---
+++ Data Structures & Algorithms/number-of-students-unable-to-eat-lunch/submission-1.ts
@@ -0,0 +1,36 @@
+class Solution {
+    /**
+     * @param {number[]} students
+     * @param {number[]} sandwiches
+     * @return {number}
+     */
+    countStudents(students: number[], sandwiches: number[]): number {
+        // circle sandwich === 0, square sandwich === 1
+        // -- using frequency count --
+        // count number of students preferring each sandwich type
+        // process sandwiches in order
+        // 1. if at least one student wants the current sandwich, 
+        //    decr counter and total remaining
+        // 2. if no student wants, stop immediately since those behind cant be served
+        // return the remaining count
+        let queue = students.length
+        const count = new Int32Array(2) // create array of size 2
+
+        // loop over students -- building freq counter
+        for(let student of students){
+            count[student]++
+        }
+
+        // loop over sandwiches -- using algo
+        for(let sandwich of sandwiches){
+            if(count[sandwich] > 0){
+                count[sandwich]--
+                queue--
+            } else {
+                break
+            }
+        }
+
+        return queue
+    }
+}
